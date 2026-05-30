8d57d6956e47507d69292c271b0551b95327edea
Yuriy Mishchuk <yuramishchuk1703@gmail.com>
2026-05-30 20:20:39 +0300

    fixed create Rg with tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,26 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
-  type = string
+  type        = string
 }
 
 variable "location" {
   description = "Azure region"
-  type = string
+  type        = string
   default     = "eastus"
 }
 
 variable "storage_account_name" {
   description = "Name of the storage account"
-  type = string
+  type        = string
 }
 
 variable "container_name" {
   description = "Name of the storage container"
-  type = string
+  type        = string
 }
 
 variable "blob_name" {
   description = "Name of the blob file"
-  type = string
+  type        = string
 }
 
